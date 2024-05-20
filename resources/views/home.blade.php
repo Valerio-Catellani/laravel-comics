83cc9f8b9727d561b0d9041b79e9172e197ba4f6
Valerio-Catellani <valeriocatellani@gmail.com>
2024-05-20 17:25:05 +0200

    created the main section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,25 @@
 @section('title', 'Home')
 
 @section('content')
-    <main>
-        <h1>Home page</h1>
-        <ul>
-            @foreach ($data['comics'] as $comic)
-                <li>{{ $comic['series'] }}</li>
-            @endforeach
-        </ul>
+    <main class="text-white d-flex flex-column align-items-center justify-content-center ">
+        <div class="jumbotron-container">
+            <img class="vw-100 img-fluid" src="{{ Vite::asset('resources/img/jumbotron.jpg') }}" alt="Jumbotron">
+        </div>
+        <div class="container">
+            <div role="button" class="mine-button text-center py-2 m-4 align-self-center" id="current-series">CURRENT SERIES
+            </div>
+            <div class="card-container row gap-3 py-1 justify-content-center ">
+                @foreach ($data['comics'] as $comic)
+                    <div class="card p-1 col-5 col-md-6 col-lg-3 col-xl-2 mb-4 text-black" role="button">
+                        <div class="img-container">
+                            <img class="img-fluid" src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                        </div>
+                        <h3 class="mb-0 mt-3">{{ strtoupper($comic['series']) }}</h3>
+                    </div>
+                @endforeach
+            </div>
+            <div role="button" class="mine-button text-center py-2 m-4 align-self-center" id="load-more">LOAD MORE</div>
+        </div>
     </main>
 
 @endsection
